5b291d34017a0111c2511b6b23dd1186d98ab42a
Sasarino MARi <develop.mari@gmail.com>
2022-07-06 23:46:25 +0900

    네트워크 오류시 재귀 횟수 제한

--- android/app/src/main/java/com/sasarinomari/spcmconsole/network/gateway/GatewayBase.kt
+++ android/app/src/main/java/com/sasarinomari/spcmconsole/network/gateway/GatewayBase.kt
@@ -9,16 +9,32 @@ internal abstract class GatewayBase {
     abstract class GeneralHandler<T> (private val client: APIClient,
                          private val callback: ((T)->Unit)?,
                          private val recursive: () -> Unit): Callback<T> {
+
+        var recursiveCount = 0
+
         override fun onResponse(call: Call<T>, response: Response<T>) {
             when {
                 response.isSuccessful -> callback?.let { it(response.body()!!) }
-                response.code() == 403 -> { client.disconnect(); recursive() }
-                else -> { client.disconnect(); client.error("요청 실패(${response.code()}) : ${response.message()}") }
+
+                response.code() == 403 -> {
+                    client.disconnect()
+
+                    if (recursiveCount++ == 0)
+                        recursive()
+                }
+
+                else -> {
+                    client.disconnect()
+                    client.error("요청 실패(${response.code()}) : ${response.message()}")
+
+                    if (recursiveCount++ == 0)
+                        recursive()
+                }
             }
         }
 
         override fun onFailure(call: Call<T>, t: Throwable) {
-            client.disconnect();
+            client.disconnect()
             client.error("네트워크 오류: ${t.message}")
         }
     }
